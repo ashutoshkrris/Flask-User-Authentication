6c2a82885ec3990d2c6a017c660452bf6c8ca709
Ashutosh Krishna <ashutoshbritish@gmail.com>
2022-08-30 19:39:12 +0530

    Add command to create admin

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 from flask.cli import FlaskGroup
 
-from src import app
+from src import app, db
+from src.accounts.models import User
 
 import unittest
+import getpass
 
 
 cli = FlaskGroup(app)
@@ -18,6 +20,21 @@ def test():
     else:
         return 1
 
+@cli.command('create_admin')
+def create_admin():
+    """Creates the admin user."""
+    email = input("Enter email address: ")
+    password = getpass.getpass("Enter password: ")
+    confirm_password = getpass.getpass("Enter password again: ")
+    if password != confirm_password:
+        print("Passwords don't match")
+        return 1
+    try:
+        user = User(email=email, password=password, is_admin=True)
+        db.session.add(user)
+        db.session.commit()
+    except Exception:
+        print("Couldn't create admin user.")
 
 if __name__ == "__main__":
     cli()
